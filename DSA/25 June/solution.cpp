547c36c88f4fd67efaace813bd38e73d9b26dbfe
mrsumitr <152645784+mrsumitr@users.noreply.github.com>
2025-06-27 13:51:34 +0530

    Update solution.cpp

--- DSA/25 June/solution.cpp
+++ DSA/25 June/solution.cpp
@@ -30,4 +30,34 @@ public:
     }
 };
 
-#Q2 cyclic
+#Q2 cyclic check
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL){
+            return false;
+        }
+
+        unordered_map<ListNode*,int> mp;
+
+        ListNode* temp=head;
+        while(temp!=NULL){
+            if(mp.find(temp)!=mp.end()){
+                return true;
+            }
+            mp[temp]++;
+            temp=temp->next;
+
+        }
+        return false;
+
+    }
+};
