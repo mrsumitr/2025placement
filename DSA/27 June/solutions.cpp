efe44720e14b6a4391224d0575b62ffa8ac5e736
mrsumitr <152645784+mrsumitr@users.noreply.github.com>
2025-06-29 23:30:41 +0530

    Create solutions.cpp

---
+++ DSA/27 June/solutions.cpp
@@ -0,0 +1,87 @@
+#Q1 Add Two Numbers
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyHead = new ListNode(0);  // dummy node
+        ListNode* curr = dummyHead;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry > 0) {
+            int sum = carry;
+
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+
+        return dummyHead->next;
+    }
+};
+#Q2 Maximum Twin Sum
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        while (head) {
+            ListNode* nextNode = head->next;
+            head->next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+public:
+    int pairSum(ListNode* head) {
+        // Step 1: Find the middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode* secondHalf = reverseList(slow);
+
+        // Step 3: Compare values and calculate max twin sum
+        int maxSum = 0;
+        ListNode* firstHalf = head;
+        while (secondHalf) {
+            maxSum = max(maxSum, firstHalf->val + secondHalf->val);
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+
+        return maxSum;
+    }
+};
