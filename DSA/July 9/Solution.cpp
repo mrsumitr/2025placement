5e65e82f3fe5567fad7a1d1706efa24e82f9ce64
mrsumitr <152645784+mrsumitr@users.noreply.github.com>
2025-07-12 22:44:52 +0530

    Update Solution.cpp

--- DSA/July 9/Solution.cpp
+++ DSA/July 9/Solution.cpp
@@ -44,4 +44,56 @@ public:
     }
 };
 #Q3 198. House Robber
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[n - 1];
+    }
+};
+
+
+
 #Q4 199. House Robber II
+class Solution {
+public:
+    int robRange(vector<int>& nums, int start, int end) {
+        int n = end - start + 1;
+        if (n == 1) return nums[start];
+        
+        vector<int> dp(n);
+        dp[0] = nums[start];
+        dp[1] = max(nums[start], nums[start + 1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(dp[i - 1], nums[start + i] + dp[i - 2]);
+        }
+
+        return dp[n - 1];
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        // Case 1: Rob from 0 to n-2
+        int case1 = robRange(nums, 0, n - 2);
+
+        // Case 2: Rob from 1 to n-1
+        int case2 = robRange(nums, 1, n - 1);
+
+        return max(case1, case2);
+    }
+};
