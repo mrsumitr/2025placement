1e8b5ff58ad549126849f42a4f6ce100341a8168
mrsumitr <152645784+mrsumitr@users.noreply.github.com>
2025-07-20 08:58:25 +0530

    Update soln.cpp

--- DSA/17 July/soln.cpp
+++ DSA/17 July/soln.cpp
@@ -19,7 +19,50 @@ public:
         return (left != nullptr) ? left : right;
     }
 };
-#Q2 
+#Q2 2385. Amount of Time for Binary Tree to Be Infected
+class Solution {
+private:
+    // Standard height function
+    int height(TreeNode* root) {
+        if (!root) return 0;
+        return 1 + max(height(root->left), height(root->right));
+    }
+
+    // Modified to return distance if found, else -1
+    int isthere(TreeNode* root, int start, int& maxTime) {
+        if (!root) return -1;
+
+        if (root->val == start) {
+            // Return height of this subtree (spread time downwards)
+            maxTime = max(maxTime, height(root) - 1);
+            return 0;  // distance from start to itself
+        }
+
+        int left = isthere(root->left, start, maxTime);
+        if (left != -1) {
+            // Infection can go to the right subtree
+            maxTime = max(maxTime, left + 1 + height(root->right));
+            return left + 1;
+        }
+
+        int right = isthere(root->right, start, maxTime);
+        if (right != -1) {
+            // Infection can go to the left subtree
+            maxTime = max(maxTime, right + 1 + height(root->left));
+            return right + 1;
+        }
+
+        return -1;  // Not found in this subtree
+    }
+
+public:
+    int amountOfTime(TreeNode* root, int start) {
+        int maxTime = 0;
+        isthere(root, start, maxTime);
+        return maxTime;
+    }
+};
+
 #Q3 222. Count Complete Tree Nodes
 /**
  * Definition for a binary tree node.
@@ -49,4 +92,38 @@ public:
         return count;
     }
 };
-#Q4 
+#Q4 98. Validate Binary Search Tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void inorder(TreeNode* root, vector<int> &check){
+        if(!root) return;
+        inorder(root->left,check);
+        check.push_back(root->val);
+        inorder(root->right,check);
+    }
+public:
+
+    bool isValidBST(TreeNode* root) {
+       vector<int> check;
+       inorder(root,check);
+       for(int i=0; i<check.size()-1; i++){
+        if(check[i]>=check[i+1]){
+            return false;
+        }
+        
+       }
+       return true;
+    }
+
+};
